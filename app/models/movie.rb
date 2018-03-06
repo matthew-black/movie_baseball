9105e56f535ca76206a8beb391a7fc9d2713a805
Matthew Black <matthewallenblack@gmail.com>
2018-03-06 15:19:59 -0600

    Change how first movie is found. Can select by year now.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,12 +8,16 @@ class Movie
     @title = Tmdb::Movie.detail(id).title
     @image = "http://image.tmdb.org/t/p/w300" + Tmdb::Movie.posters(id)[0].file_path
     @cast  = Tmdb::Movie.cast(id).map do |actor|
-      {id: actor.id, name: actor.name}
+      {actor.id => actor.name}
     end
   end
 
   def self.fetch_popular_movie_id
-    Tmdb::Movie.popular.results.sample.id
+    # Tmdb::Movie.popular.results.sample.id
+    random_year = rand(2006..2010)
+    called = Tmdb::Discover.movie(primary_release_year: random_year, sort_by: 'revenue.desc')
+
+    called.results.sample.id
   end
 
 end
